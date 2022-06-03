0bc8670f8d2eb30732ef9fc91d7c175d7f8acaff
yooonyng <yooonyng@gmail.com>
2022-06-03 16:36:26 +0900

    0603

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def main():
+
+    menu = ['home']
+    choice = st.sidebar.selectbox(menu)
+
+    if choice == menu[0]:
+        pass
+    elif choice == menu[1]:
+        pass
+    elif choice == menu[2]:
+        pass
+    elif choice == menu[3]:
+        pass
+    
+if __name__ == '__main__':
+    main()
