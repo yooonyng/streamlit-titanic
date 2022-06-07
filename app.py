96b0ebd6b4193f983e6c01aa988ba72f4d753e83
yooonyng <yooonyng@gmail.com>
2022-06-07 17:37:09 +0900

    0607

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 import joblib
-import sklearn
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import altair as alt
+import plotly.express as px
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+import base64
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.offline as py
+
+night_colors = ['lightskyblue', 'navy', 'snow','aliceblue']
 
 
 def main():
     df = pd.read_csv('data/titanic_train.csv')
-    st.subheader('타이타닉 생존 여부 확인하기')
-    print(sklearn.__version__)
+
+    st.title('🚢타이타닉 생존 여부 확인하기')
+    st.text('\n')
+
+    image = Image.open('data/img01.jpg')
+    st.image(image)
+    st.text('\n')
 
     with st.sidebar:
+        st.title('🔍생존자 확인하기')
+        st.write('---')
 
         pclass = st.sidebar.selectbox('티켓 클래스를 선택하세요', [1, 2, 3])
         st.subheader('\n')
@@ -41,9 +60,8 @@ def main():
         st.subheader('\n')
         fare = (st.number_input('탑승 요금을 입력하세요',0))/1257
         # 달러 환전
-
-
         new_data = np.array([pclass,sex,age,sibsp,parch,fare,embarked])
+
     
 
     if st.button('생존 확인') :
@@ -54,40 +72,87 @@ def main():
 
         new_data = np.array([new_data])
         X_new = scaler_X.transform(new_data)
-        
         y_pred = classifier.predict(X_new)
 
+        # 생존 = 1
+        # 생존 아님 = 0
+
         if y_pred[0] == 1 :
-            st.text('살았따.')
+            st.subheader('살았따,,')
+            # image = Image.open('data/img03.gif')
+            # st.image(image)
+            file_ = open("data/img03.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
         elif y_pred[0] == 0 :
-            st.text('죽었따.')
+            st.subheader('죽었따.')
+            # image = Image.open('data/img02.gif')
+            # st.image(image)
+            file_ = open("data/img02.gif", "rb")
+            contents = file_.read()
+            data_url = base64.b64encode(contents).decode("utf-8")
+            file_.close()
+        st.markdown(
+            f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+            unsafe_allow_html=True,)
         
+    st.write('---')
 
 
+    st.subheader('*️⃣나이와 생존과의 관계')
+    fig = px.histogram(df, x='Age',  color_discrete_sequence=['lightskyblue'], barmode='overlay')
+    fig.update_layout(title="Age")
+    st.write(fig)
+    st.subheader('\n')
+
+    st.subheader('*️⃣성별과 생존과의 관계')
+    labels = [x for x in df.Sex.value_counts().index]
+    values = df.Sex.value_counts()
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,pull=[0.03, 0])])
+    fig.update_layout(title_text="Gender")
+    fig.update_traces(marker=dict(colors=night_colors))
+    st.write(fig)
+    st.subheader('\n')
 
 
+    st.subheader('*️⃣선착장과 생존과의 관계')
+    labels = [x for x in df.Embarked.value_counts().index]
+    values = df.Embarked.value_counts()
+    fig=go.Figure(data=[go.Pie(labels=["Southampton","Cherbourg","Queenstown"],values=values,hole=.3,pull=[0,0,0.06,0])])
+    fig.update_layout(title_text="Embarked")
+    fig.update_traces(marker=dict(colors=night_colors))
+    st.write(fig)
+
+    fig = make_subplots(rows=1, cols=3, specs=[[{"type": "pie"}, {"type": "pie"}, {"type": "pie"}]])
+    fig.add_trace(go.Pie(labels=df.loc[df['Embarked'] == 'C']['Survived'], pull = [.1, .1],
+                    title = 'Embarked C vs. Survived'), row=1, col=1)
+    fig.add_trace(go.Pie(labels=df.loc[df['Embarked'] == 'S']['Survived'], pull = [.07, .07],
+                    title = 'Embarked S vs. Survived'),row=1, col=2)
+    fig.add_trace(go.Pie(labels=df.loc[df['Embarked'] == 'Q']['Survived'], pull = [.1, .1],
+                    title = 'Embarked Q vs. Survived'), row=1, col=3)
+    fig.update_traces(marker=dict(colors=night_colors))
+    st.write(fig)
 
 
-
-    # st.sidebar.header('Menu')
-    # st.sidebar.markdown("Drag the sliders")
-
-    # if st.checkbox("Show original dataset"):
-    #  st.write(df.iloc[0:age])   
-
-    # column_list = df.columns
-    # choice_list = st.multiselect('컬럼을 선택하세요',column_list)
-    # df[choice_list]
-    # st.dataframe(print(choice_list))
+    st.subheader('*️⃣요금과 생존과의 관계')
+    fig = px.scatter(data_frame = df
+        ,x = 'Fare'
+        ,y = 'Age'
+        ,color = 'Survived',
+        size='Parch',
+        hover_data=['Sex', 'Age'],
+        color_discrete_sequence =['navy','#57A7F3','#D3DBDD'],
+        title="Age vs Fare")
+    st.write(fig)
+    st.subheader('\n')
     
-    # pclass = [[pclass]]
-    # sex =[[sex]]
-    # encoder.fit_transform(sex)
-    # encoder.fit_transform(pclass)
-    # new_data = np.array([pclass,sex,age,embarked],dtype=object)
-    # new_data2 = np.vectorize(new_data)
-    # X_new = scaler_X.transform(new_data)
-    # X_new = X_new.toarray()
+    
+
+
+
+
+
 
     
 if __name__ == '__main__':
