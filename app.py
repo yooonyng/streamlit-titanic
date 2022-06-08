af628079b5cb6efcd152fd933c9fd7ee191fdcdc
yooonyng <yooonyng@gmail.com>
2022-06-08 10:24:59 +0900

    0608

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def main():
         # 생존 아님 = 0
 
         if y_pred[0] == 1 :
-            st.subheader('당신은 살았습니다.')
+            st.subheader('\"당신은 살았습니다.\"')
             # image = Image.open('data/img03.gif')
             # st.image(image)
             # file_ = open("data/img03.gif", "rb")
@@ -86,7 +86,7 @@ def main():
             # data_url = base64.b64encode(contents).decode("utf-8")
             # file_.close()
         elif y_pred[0] == 0 :
-            st.subheader('당신은 죽었습니다.')
+            st.subheader('\"당신은 죽었습니다.\"')
             # image = Image.open('data/img02.gif')
             # st.image(image)
             # file_ = open("data/img02.gif", "rb")
@@ -97,30 +97,55 @@ def main():
         #     f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
         #     unsafe_allow_html=True,)
         
-    st.write('---')
-
+    st.subheader('\n')
+    st.subheader('\n')
+    st.subheader('\n')
+    st.subheader('\n')
 
     st.subheader('*️⃣나이와 생존과의 관계')
-    fig = px.histogram(df, x='Age',  color_discrete_sequence=['#1d29d1'], barmode='overlay')
-    fig.update_layout(title="Age")
-    st.write(fig)
+    st.write('---')
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader('*\"나이가 어릴수록 생존율이 높을까?\"*')
+        image = Image.open('data/img04.jpg')
+        st.image(image)
+    with col2:
+        fig = px.histogram(df, x='Age',  color_discrete_sequence=['#1d29d1'], barmode='overlay')
+        fig.update_layout(title="Age")
+        st.write(fig)
+    st.subheader('\n')
+    st.subheader('\n')
     st.subheader('\n')
 
-    st.write('---')
+
 
     st.subheader('*️⃣성별과 생존과의 관계')
-    labels = [x for x in df.Sex.value_counts().index]
-    values = df.Sex.value_counts()
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,pull=[0.03, 0])])
-    fig.update_layout(title_text="Gender")
-    fig.update_traces(marker=dict(colors=night_colors))
-    st.write(fig)
+    st.write('---')
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader('*\"여자승객 74%, 어린이 52%, 남자승객 20% 구조\"*')
+        st.text('1등석 승객 남성은 70%가 사망\n2등석 승객 남성은 90%가 사망')
+        st.text('당시 1,2등석에 탄 남자 승객들은\n미국,영국의 최상류층이었다.')
+        image = Image.open('data/img05.jpg')
+        st.image(image)
+        st.caption('Titanic Inquiry Project')
+
+    with col2:
+        labels = [x for x in df.Sex.value_counts().index]
+        values = df.Sex.value_counts()
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3,pull=[0.03, 0])])
+        fig.update_layout(title_text="Gender")
+        fig.update_traces(marker=dict(colors=night_colors))
+        st.write(fig)
+    st.subheader('\n')
+    st.subheader('\n')
     st.subheader('\n')
 
-    st.write('---')
 
 
     st.subheader('*️⃣선착장과 생존과의 관계')
+    st.write('---')
+    
     labels = [x for x in df.Embarked.value_counts().index]
     values = df.Embarked.value_counts()
     fig=go.Figure(data=[go.Pie(labels=["Southampton","Cherbourg","Queenstown"],values=values,hole=.3,pull=[0,0,0.06,0])])
@@ -137,24 +162,50 @@ def main():
                     title = 'Embarked Q vs. Survived'), row=1, col=3)
     fig.update_traces(marker=dict(colors=night_colors))
     st.write(fig)
+    st.subheader('\n')
+    st.subheader('\n')
+    st.subheader('\n')
 
-    st.write('---')
+
 
 
     st.subheader('*️⃣요금과 생존과의 관계')
-    fig = px.scatter(data_frame = df
-        ,x = 'Fare'
-        ,y = 'Age'
-        ,color = 'Survived',
-        size='Parch',
-        hover_data=['Sex', 'Age'],
-        title="Age vs Fare")
-    st.write(fig)
+    st.write('---')
+    col1,col2 = st.columns(2)
+    with col1:
+        st.subheader('*\"비싼 요금을 냈다면 생존율이 높을까?\"*')
+        st.text('사고로 1,514명이 사망했고\n710명이 구조되었습니다.')
+        image = Image.open('data/img06.jpg')
+        st.image(image)
+        st.caption('타이타닉호의 침몰, 또 다른 이야기: http://memorialnews.net/news/article.html?no=11768')
+    with col2:
+        fig = px.scatter(data_frame = df
+            ,x = 'Fare'
+            ,y = 'Age'
+            ,color = 'Survived',
+            size='Parch',
+            hover_data=['Sex', 'Age'],
+            title="Age vs Fare")
+        st.write(fig)
     st.subheader('\n')
+    st.subheader('\n')
+    st.subheader('\n')
+
     
     
 
-
+    # st.subheader('2. 최근 소년범죄 이슈')
+    # col1,col2,col3 = st.columns(3)
+    # with col1:
+    #     gif_html = get_img_with_href('data/sum01.png', 'https://www.youtube.com/watch?v=h-WmvzCtRh0')
+    #     st.markdown(gif_html, unsafe_allow_html=True)
+    # with col2:
+    #     pass
+    # with col3:
+    #     st.subheader('끊임없는 촉법소년 논란 연령하향 해야 vs 처벌이 능사는 아냐')
+    #     st.text('#촉법소년 #소년법 #촉법소년범죄')
+    # st.text('\n')
+    # st.text('\n')
 
 
 
